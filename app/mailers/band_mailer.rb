b64dec8fede492b72d5101d8234a2571169269fe
Thomas Geirhovd <thomas@linted.net>
2011-03-22 21:12:40 +0100

    mail on musicvid contest

--- app/mailers/band_mailer.rb
+++ app/mailers/band_mailer.rb
@@ -8,4 +8,14 @@ class BandMailer < ActionMailer::Base
       mail(:to => band.email, :subject => 'Spørsmål til bandpresentasjon').deliver
     end
   end
+  def musicvideo_email
+    mail(:to => "tg90nor@gmail.com", :subject => 'Musikkvideo-konkurranse').deliver
+    mail(:to => "susann.stave@rockmotrus.no", :subject => 'Musikkvideo-konkurranse').deliver
+    mail(:to => "cato@rockmotrus.no", :subject => 'Musikkvideo-konkurranse').deliver
+    bands = Band.all
+    bands.each do |band|
+      @band = band
+      mail(:to => band.email, :subject => 'Musikkvideo-konkurranse').deliver
+    end
+  end
 end
