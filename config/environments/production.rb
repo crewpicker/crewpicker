1aad5d57d572ac4442b94f608b48c2331e29cb1c
Thomas Geirhovd <thomas@linted.net>
2011-03-22 19:57:32 +0100

    production mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,5 @@ FestivalAdmin::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "localhost",
-    :port                 => 25,
-    :authentication       => nil
-  }
+  config.action_mailer.delivery_method = :sendmail
 end
