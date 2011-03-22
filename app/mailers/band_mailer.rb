49a59bf7a267d07ac3bb458138bffac26647ec94
Thomas Geirhovd <thomas@linted.net>
2011-03-22 20:04:37 +0100

    sende bandmail

--- app/mailers/band_mailer.rb
+++ app/mailers/band_mailer.rb
@@ -1,11 +1,11 @@
 class BandMailer < ActionMailer::Base
   default :from => "Rock Mot Rus <festival@rockmotrus.no>"
   def extrainfo_email
-    mail(:to => "tg90nor@gmail.com", :subject => 'Spørsmål til bandpresentasjon')
+    mail(:to => "tg90nor@gmail.com", :subject => 'Spørsmål til bandpresentasjon').deliver
     bands = Band.all
     bands.each do |band|
       @band = band
-      mail(:to => band.email, :subject => 'Spørsmål til bandpresentasjon')
+      mail(:to => band.email, :subject => 'Spørsmål til bandpresentasjon').deliver
     end
   end
 end
