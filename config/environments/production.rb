7f6bd1a25b98043ee1f3baae5873618804a5796c
Thomas Geirhovd <thomas@linted.net>
2011-03-22 19:35:08 +0100

    production mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,13 @@ FestivalAdmin::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "mail.rockmotrus.no",
+    :port                 => 25,
+    :domain               => 'rockmotrus.no',
+    :user_name            => 'festival@rockmotrus.no',
+    :password             => '***REMOVED***',
+    :authentication       => 'plain',
+    :enable_starttls_auto => false
 end
