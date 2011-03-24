c4afeaf2fb88d75eebaecd16a2217f198fa3a86f
Thomas Geirhovd <thomas@linted.net>
2011-03-24 15:20:30 +0100

    stage schedules

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,4 +3,5 @@ class Band < ActiveRecord::Base
   include UuidHelper
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_many :band_members
+  has_one :stage_schedule
 end
