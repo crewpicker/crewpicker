88d233d2f9523b32e1cb284a2249a6e952aff29c
Thomas Geirhovd <thomas@linted.net>
2011-03-28 12:38:24 +0200

    added fireguards

---
+++ db/migrate/20110327174214_create_location_schedules.rb
@@ -0,0 +1,17 @@
+class CreateLocationSchedules < ActiveRecord::Migration
+  def self.up
+    create_table :location_schedules do |t|
+      t.integer :location_id
+      t.integer :volunteer_id
+      t.integer :slot
+      t.datetime :from
+      t.datetime :to
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :location_schedules
+  end
+end
