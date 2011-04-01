83fb86e2508865dc6ef71fc1a6e776e508d2eae3
Thomas Geirhovd <thomas@linted.net>
2011-04-01 21:11:51 +0200

    PDFkit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  def nl2br(s)
+    s.gsub(/\n/, "<br>").html_safe
+  end
+  def pdf_image_path(image)
+    return File.expand_path(Rails.root) + '/public/images/' + image
+  end
 end
