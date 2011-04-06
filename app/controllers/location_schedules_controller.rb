74dedccb4c7bd5af89d004a799a15e88daf0aaae
Thomas Geirhovd <thomas@linted.net>
2011-04-06 22:42:40 +0200

    Fix location schedules, improved access card generation, volunteer age display

--- app/controllers/location_schedules_controller.rb
+++ app/controllers/location_schedules_controller.rb
@@ -1,43 +1,39 @@
 class LocationSchedulesController < ApplicationController
-  def get_events
+  def show
     events = []
-    LocationSchedule.all(:conditions => [ "location_id = ? AND slot = ?", params[:id], params[:slot]]).each do |location_schedule|
+    LocationSchedule.all(:conditions => [ "location_id = ? AND slot = ?", params[:location_id], params[:id]]).each do |location_schedule|
       events << {:id => location_schedule.id, :title => location_schedule.fireguard.name, :start => "#{location_schedule.from.iso8601}", :end => "#{location_schedule.to.iso8601}", :allDay => false, :recurring => false}
     end
     render :json => events.to_json
   end
-  def show_schedule
+  def index
     @events = Fireguard.find(:all, :order => "name")
-    if params[:id]
-      @location = Location.find(params[:id])
-    elsif params[:name]
-      @location = Location.find_by_name(params[:name].capitalize)
-    end
+    @location = Location.find(params[:location_id])
   end
   def create
     fireguard = Fireguard.find(params[:fireguard_id])
-    location = Location.find(params[:id])
+    location = Location.find(params[:location_id])
     location_schedule = LocationSchedule.new
     location_schedule.from = DateTime.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i, params[:hour].to_i, params[:minute].to_i)
     location_schedule.to = DateTime.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i, params[:hour].to_i, params[:minute].to_i)
     location_schedule.to = location_schedule.to.advance(:minutes => 120)
-    location_schedule.slot = params[:slot]
+    location_schedule.slot = params[:id]
     fireguard.location_schedules << location_schedule
     location.location_schedules << location_schedule
     location_schedule.save
     render :json => {:id => location_schedule.id.to_json}
   end
-  def move
+  def update
     location_schedule = LocationSchedule.find(params[:event_id])
     location_schedule.from = location_schedule.from.advance(:days => params[:day_delta].to_i, :minutes => params[:minute_delta].to_i)
     location_schedule.to = location_schedule.to.advance(:days => params[:day_delta].to_i, :minutes => params[:minute_delta].to_i)
-    location_schedule.slot = params[:slot]
+    location_schedule.slot = params[:id]
     location_schedule.save
   end
-  def delete
+  def destroy
     location_schedule = LocationSchedule.find(params[:event_id])
     location_schedule.destroy
-    slot = params[:slot]
+    slot = params[:id]
     render :update do |page|
       page<<"$('#calendar-#{slot}').fullCalendar( 'refetchEvents' )"
     end
