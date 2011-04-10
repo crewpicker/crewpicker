b10d0cd5777355bc11ac6be87ed3ce5ba8d7df19
Thomas Geirhovd <thomas@linted.net>
2011-04-10 20:53:26 +0200

    Band member overview list sorted by person name

--- app/models/band_member.rb
+++ app/models/band_member.rb
@@ -1,6 +1,6 @@
 class BandMember < ActiveRecord::Base
   belongs_to :band
   belongs_to :person
-  default_scope :order => "band_members.name"
+  default_scope :include => :person, :order => "people.name"
   accepts_nested_attributes_for :person
 end
