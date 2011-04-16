359a6aaf909fa77f3cad6ad35d8eca6204106d2d
Thomas Geirhovd <thomas@linted.net>
2011-04-16 09:17:41 +0200

    Moar adjustments

--- app/controllers/flightscreens_controller.rb
+++ app/controllers/flightscreens_controller.rb
@@ -1,6 +1,6 @@
 class FlightscreensController < ApplicationController
   def show
-    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.to >= ?", DateTime.now.advance(:hours => 1, :minutes => 45)], :limit => 5)
+    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.to >= ?", DateTime.now.advance(:hours => 1, :minutes => 35)], :limit => 5)
     render :layout => false
   end
 end
