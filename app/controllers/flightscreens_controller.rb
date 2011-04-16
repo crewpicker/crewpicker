f6bbc42b4a3aa12a436a2182fc18db5cacdfb4db
Thomas Geirhovd <thomas@linted.net>
2011-04-16 06:21:20 +0200

    Adjust flightstreen: ahead of schedule

--- app/controllers/flightscreens_controller.rb
+++ app/controllers/flightscreens_controller.rb
@@ -1,6 +1,6 @@
 class FlightscreensController < ApplicationController
   def show
-    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.to >= ?", DateTime.now.advance(:hours => 2)], :limit => 5)
+    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.to >= ?", DateTime.now.advance(:hours => 2, :minutes => 15)], :limit => 5)
     render :layout => false
   end
 end
