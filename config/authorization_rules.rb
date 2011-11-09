ce8c873822d62592fe6e5cd21e17cee866da2818
Thomas Geirhovd <thomas@linted.net>
2011-11-09 22:41:52 +0100

    Enable god-mode

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,7 +1,7 @@
 authorization do
   role :admin do
     includes :groupleader
-    #has_permission_on [:users, :crew_applications], :to => [:manage]
+    has_permission_on [:users, :crew_applications], :to => [:manage]
   end
 
   role :groupleader do
