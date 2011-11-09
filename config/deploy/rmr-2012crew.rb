5321eed82fcc1edec12ad1fe5e4bca32285f55c2
Thomas Geirhovd <thomas@linted.net>
2011-11-09 06:12:02 +0100

    add db link task for rmr2012

--- config/deploy/rmr-2012crew.rb
+++ config/deploy/rmr-2012crew.rb
@@ -3,3 +3,9 @@ set :subdomain, "2012crew"
 set :deploy_to, "/srv/#{domain}/#{subdomain}/#{application}"
 
 server '2012crew.rockmotrus.no', :app, :web, :db, :primary => true
+
+after "deploy:update_code", "dblink"
+
+task :dblink do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/db/db_production #{release_path}/db/db_production"
+end
