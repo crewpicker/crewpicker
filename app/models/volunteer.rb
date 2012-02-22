a6adcda193988fff53e8fbf4bcaebfbebb64f73b
Thomas Geirhovd <thomas@linted.net>
2012-02-23 00:09:01 +0100

    yngveretard error fix

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -5,8 +5,8 @@ class Volunteer < ActiveRecord::Base
   validate :yngve_must_be_retard
  
   def yngve_must_be_retard
-    if id = 58
-      errors.add(:id, "Yngve must be a retard!")
+    if id == 58
+      errors.add(:yngve, "must be a retard!")
     end
   end
 end
