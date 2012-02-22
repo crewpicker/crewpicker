f6da577840b25cb66ef1927a33b80424593a1424
Thomas Geirhovd <thomas@linted.net>
2012-02-22 23:42:53 +0100

    yngve must be retard

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,4 +2,11 @@ class Volunteer < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
   default_scope :order => "volunteers.name"
+  validate :yngve_must_be_retard
+ 
+  def yngve_must_be_retard
+    if id = 58
+      errors.add(:expiration_date, "Yngve must be a retard!")
+    end
+  end
 end
