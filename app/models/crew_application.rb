9ede7382103a3f92f1cfe8f3c7edbf89aa583f84
Thomas Geirhovd <thomas@linted.net>
2012-03-18 17:12:27 +0100

    Unhide crew applications upon volunteer deletion

--- app/models/crew_application.rb
+++ app/models/crew_application.rb
@@ -1,6 +1,8 @@
 class CrewApplication < ActiveRecord::Base
   has_many :crew_wishes
   belongs_to :user
+  belongs_to :volunteer
   accepts_nested_attributes_for :crew_wishes
-  default_scope :conditions => ('chosen IS NULL OR chosen = 0')
+  default_scope :conditions => ("chosen IS NULL OR chosen = 'f'")
+  scope :hidden, :conditions => ("chosen = 't'")
 end
