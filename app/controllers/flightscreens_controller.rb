80ec8a8e4d0d6e1cdcdb3194bd260ae1c3d7aa86
Thomas Geirhovd <thomas@linted.net>
2012-03-30 22:51:08 +0200

    edit stage schedules

--- app/controllers/flightscreens_controller.rb
+++ app/controllers/flightscreens_controller.rb
@@ -1,6 +1,6 @@
 class FlightscreensController < ApplicationController
   def show
-    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.end >= ?", DateTime.now.advance(:minutes => 30)], :limit => 3)
+    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.end >= ?", DateTime.now], :limit => 3)
     render :layout => false
   end
 end
