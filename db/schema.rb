b55e9e5401a3a7dd364746d1644e6f402e4ab520
Thomas Geirhovd <thomas@linted.net>
2013-01-11 11:06:34 +0100

    Rails upgrade leads to db schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,16 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
 
   create_table "access_levels", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "ad_types", :force => true do |t|
     t.string   "name"
     t.integer  "value"
     t.string   "invoice_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "ads", :force => true do |t|
@@ -35,24 +35,24 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.integer  "ad_type_id"
     t.text     "address"
     t.text     "notes"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",    :null => false
     t.string   "provider",   :null => false
     t.string   "uid",        :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "band_members", :force => true do |t|
     t.string   "band_id",    :limit => 36
     t.string   "name"
     t.string   "role"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
     t.integer  "person_id"
     t.integer  "user_id"
   end
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.integer  "user_id"
   end
 
@@ -88,8 +88,8 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.text     "experience"
     t.integer  "volunteer_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.boolean  "chosen"
   end
 
@@ -97,8 +97,8 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.integer  "crew_application_id"
     t.integer  "group_id"
     t.integer  "priority"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
   end
 
   create_table "fireguards", :force => true do |t|
@@ -110,15 +110,15 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.string   "email"
     t.text     "notes"
     t.boolean  "active"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.boolean  "internal"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.text     "description"
   end
 
@@ -128,15 +128,15 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.integer  "slot"
     t.datetime "from"
     t.datetime "to"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "locations", :force => true do |t|
     t.string   "name"
     t.integer  "slots"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "people", :force => true do |t|
@@ -144,14 +144,14 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.text     "address"
     t.string   "email"
     t.string   "phone",      :limit => 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
   end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "stage_schedules", :force => true do |t|
@@ -159,16 +159,16 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.string   "band_id",    :limit => 36
     t.datetime "start"
     t.datetime "end"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
   end
 
   create_table "stages", :force => true do |t|
     t.string   "name"
     t.datetime "from"
     t.datetime "to"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "user_roles", :force => true do |t|
@@ -182,8 +182,8 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.string   "reset_password_token"
@@ -203,8 +203,8 @@ ActiveRecord::Schema.define(:version => 20120211155048) do
     t.string   "phone"
     t.integer  "group_id"
     t.string   "access_level"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "user_id"
   end
 
