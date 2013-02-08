4c4a0db3ca0e99631231f241639788d6ce3ed555
tg90nor <tg90nor@thomasg.ns5.powertech.no>
2013-02-08 09:40:41 +0100

    Added utf-8 encoding to all models

--- app/models/ad_type.rb
+++ app/models/ad_type.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class AdType < ActiveRecord::Base
   has_many :ads
 end
--- app/models/crew_wish.rb
+++ app/models/crew_wish.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class CrewWish < ActiveRecord::Base
   belongs_to :crew_application
   belongs_to :group
