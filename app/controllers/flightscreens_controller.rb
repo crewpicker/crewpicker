6817794ca3e79e7154d1d2d643308be605d3b917
Thomas Geirhovd <thomas@linted.net>
2013-03-22 17:57:18 +0100

    Update flightscreen

--- app/controllers/flightscreens_controller.rb
+++ app/controllers/flightscreens_controller.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 class FlightscreensController < ApplicationController
   def show
-    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.end > ?", DateTime.now.to_formatted_s(:db)], :limit => 3)
+    @stage_schedules = StageSchedule.find(:all, :conditions => ["stage_schedules.end > ?", DateTime.now.to_formatted_s(:db)], :limit => 3, :order => "`id` DESC")
     render :layout => false
   end
 end
