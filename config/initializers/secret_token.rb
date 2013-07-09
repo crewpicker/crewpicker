46aba6065c4cddf2e34fd73592c4834f57a94343
Thomas Geirhovd <thomas@linted.net>
2013-07-09 22:07:26 +0200

    OH MY GOLLY GOSH I COMPLETELY FORGOT TO INCLUDE THIS IN MY RAILS 4 COMMIT!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 FestivalAdmin::Application.config.secret_token = '21b2b11fd78a7b984ad2ba91c82b66528641a6dbe365aeb52b53ca07db0c7aec0aea996af42ae42c2de2e48acae15785ffc94858a2752dcb15bab7d40f3d7874'
+FestivalAdmin::Application.config.secret_key_base = '2p1b2b11fd78a7b984ad2ba91c82b665285if76dbe365aeb52b53ca07db0ct5cc0aea996af42ae42c2de2e48acae15785ffc94858a2752dcb15bab7d40f3d7874'
