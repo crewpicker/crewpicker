83fe6dc8ade4c695614e7499c930ddbae1cd1786
Thomas Geirhovd <thomas@linted.net>
2014-01-04 02:20:18 +0100

    Allow deletion of band members, plus redirect back to band page afterwards.

--- app/models/band_member.rb
+++ app/models/band_member.rb
@@ -3,6 +3,6 @@ class BandMember < ActiveRecord::Base
   belongs_to :band
   belongs_to :person
   belongs_to :user
-  default_scope -> { joins(:person).order("people.name") }
+  default_scope -> { includes(:person).order("people.name") }
   accepts_nested_attributes_for :person
 end
