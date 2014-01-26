98ab7a89e3654bd004fedfc6094938135d6f6880
Thomas Geirhovd <thomas@linted.net>
2014-01-26 16:24:05 +0100

    Correct sender address on volunteers mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,5 +1,5 @@
 class VolunteerMailer < ActionMailer::Base
-  default from: "test@tg90nor.net"
+  default from: "Rock Mot Rus <festival@rockmotrus.no>"
   def custom_email(subject, recipients, content_markdown, content_plain)
     @content = content_markdown
     mail(:to => recipients, :subject => subject) do |format|
