584bbb75782d64e06d32307028c533a7f685c500
Thomas Geirhovd <thomas@linted.net>
2014-03-08 23:26:21 +0100

    Added option to only send crew mail to public groups

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -2,7 +2,7 @@ class VolunteerMailer < ActionMailer::Base
   default from: "Rock Mot Rus <festival@rockmotrus.no>"
   def custom_email(subject, recipients, content_markdown, content_plain)
     @content = content_markdown
-    mail(:to => recipients, :subject => subject) do |format|
+    mail(:to => 'Rock Mot Rus <festival@rockmotrus.no>', :bcc => recipients, :subject => subject) do |format|
       format.html
       format.text { render text: content_plain }
     end
