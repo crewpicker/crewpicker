4dfc88f1a1520ac0987e812f435cfa19b2807073
Thomas Geirhovd <thomas@linted.net>
2014-03-17 21:06:28 +0100

    IDK2

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,7 @@
 PDFKit.configure do |config|
   config.default_options = {
     :page_size => 'A4',
-    :disable_smart_shrinking => true,
+    :disable_smart_shrinking => false,
     :encoding=>"UTF-8"
   }
 end
