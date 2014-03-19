dd54b552701263ab1ab01bfef11e093f5e8c4725
Thomas Geirhovd <thomas@linted.net>
2014-03-19 21:11:33 +0100

    I have no idea who is next

--- app/controllers/flightscreens_controller.rb
+++ app/controllers/flightscreens_controller.rb
@@ -6,23 +6,29 @@ class FlightscreensController < ApplicationController
   end
   def now
     stage_schedule = StageSchedule.find(:all, :conditions => ["stage_schedules.end > ?", DateTime.now.to_formatted_s(:db)], :limit => 1, :order => "`id` DESC").first
-    if stage_schedule.band
-      bandname = stage_schedule.band.name or "I have no idea who is next"
-      render :text => bandname, :content_type => Mime::TEXT
+    if stage_schedule
+      bandname = stage_schedule.band.name
+    else
+      bandname = "I have no idea who is next"
     end
+    render :text => bandname, :content_type => Mime::TEXT
   end
   def next
     stage_schedule = StageSchedule.find(:all, :conditions => ["stage_schedules.end > ?", DateTime.now.to_formatted_s(:db)], :limit => 2, :order => "`id` DESC").last
-    if stage_schedule.band
-      bandname = stage_schedule.band.name or "I have no idea who is next"
-      render :text => bandname, :content_type => Mime::TEXT
+    if stage_schedule
+      bandname = stage_schedule.band.name
+    else
+      bandname = "I have no idea who is next"
     end
+    render :text => bandname, :content_type => Mime::TEXT
   end
   def later
     stage_schedule = StageSchedule.find(:all, :conditions => ["stage_schedules.end > ?", DateTime.now.to_formatted_s(:db)], :limit => 3, :order => "`id` DESC").last
-    if stage_schedule.band
-      bandname = stage_schedule.band.name or "I have no idea who is next"
-      render :text => bandname, :content_type => Mime::TEXT
+    if stage_schedule
+      bandname = stage_schedule.band.name
+    else
+      bandname = "I have no idea who is next"
     end
+    render :text => bandname, :content_type => Mime::TEXT
   end
 end
