274d6ce2599d9465959504a4ce18e610733f2bf6
Thomas Geirhovd <thomas@linted.net>
2014-09-02 22:28:40 +0200

    Remove capistrano deployment code, as it is no longer in use

--- config/deploy/rmr-2013crew.rb
+++
@@ -1,5 +0,0 @@
-set :domain, "rockmotrus.no"
-set :subdomain, "2013crew"
-set :deploy_to, "/srv/#{domain}/#{subdomain}/#{application}"
-
-server '2013crew.rockmotrus.no', :app, :web, :db, :primary => true
