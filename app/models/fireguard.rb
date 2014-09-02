00743424282776c1e66b77b96b8079bd124d2883
Thomas Geirhovd <thomas@linted.net>
2014-09-02 23:27:18 +0200

    Use new default_scope syntax

--- app/models/fireguard.rb
+++ app/models/fireguard.rb
@@ -2,5 +2,5 @@
 class Fireguard < ActiveRecord::Base
   has_many :location_schedules
   scope :active, :conditions => "fireguards.active = 't'", :order => "fireguards.name"
-  default_scope :order => "fireguards.name"
+  default_scope { order :name }
 end
