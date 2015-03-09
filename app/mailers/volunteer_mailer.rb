99504de876766dfe3957417f39fce12e3bf954de
Thomas Geirhovd <thomas@linted.net>
2015-03-10 00:08:38 +0100

    Add example config file

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,9 +1,9 @@
 class VolunteerMailer < ActionMailer::Base
   # TODO: set default from address in config file
-  default from: "***REMOVED***"
+  default from: APP_CONFIG[:default_from_email_address]
   def custom_email(subject, recipients, content_markdown, content_plain)
     @content = content_markdown
-    mail(:to => '***REMOVED***', :bcc => recipients, :subject => subject) do |format|
+    mail(to: APP_CONFIG[:default_from_email_address], bcc: recipients, subject: subject) do |format|
       format.html
       format.text { render text: content_plain }
     end
