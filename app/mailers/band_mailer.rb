8700c3ff4dabc010e9c7c838cf085003b0cddafd
Thomas Geirhovd <tg90nor@gmail.com>
2015-04-12 21:42:30 +0200

    Send confirmation email to registering bands

--- app/mailers/band_mailer.rb
+++ app/mailers/band_mailer.rb
@@ -1,12 +1,20 @@
 # encoding: UTF-8
 class BandMailer < ActionMailer::Base
-  # TODO: set default from address in config file
   default :from => APP_CONFIG[:default_from_email_address]
   def custom_email(subject, recipients, content_markdown, content_plain)
     @content = content_markdown
     mail(to: APP_CONFIG[:default_from_email_address], bcc: recipients, subject: subject) do |format|
       format.html
       format.text { render text: content_plain }
+    end
+  end
+
+  def confirmation_email(band)
+    @band = band
+    @event = Event.first
+    mail(to: band.email, subject: t(:band_signup_confirmation)) do |format|
+      format.html
+      format.text
     end
   end
 end
