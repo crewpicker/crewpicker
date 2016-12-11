a024a84dc89fa707e11ecc2ac38a2a400bb19218
Thomas Geirhovd <tg90nor@gmail.com>
2016-12-11 05:56:30 +0100

    Fixed problem with migration not adding event id because of model scope

--- db/migrate/20161211043511_add_event_to_volunteers.rb
+++ db/migrate/20161211043511_add_event_to_volunteers.rb
@@ -1,7 +1,7 @@
 class AddEventToVolunteers < ActiveRecord::Migration
   def change
     add_reference :volunteers, :event, index: true, foreign_key: true
-    if volunteers = Volunteer.where.not(event_id: nil)
+    if volunteers = Volunteer.unscoped.where.not(event_id: nil)
       volunteers.each do |volunteer|
         volunteer.event_id = ActiveEvent.first.event_id
         volunteer.save
