88bf01feccfa3514e191092c8e729d90cfd372d3
Thomas Geirhovd <tg90nor@gmail.com>
2018-08-18 23:22:27 +0200

    Stop whining if APP_CONFIG[:wkhtmltopdf_path] is unset

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = APP_CONFIG[:wkhtmltopdf_path] or '/usr/bin/wkhtmltopdf'
+  config.wkhtmltopdf = APP_CONFIG[:wkhtmltopdf_path] ? APP_CONFIG[:wkhtmltopdf_path] : '/usr/bin/wkhtmltopdf'
   config.default_options = {
     :page_size => 'A4',
     :disable_smart_shrinking => true,
