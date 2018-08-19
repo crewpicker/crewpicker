db06eb822656a859f3f612db9087e24331acd0db
Thomas Geirhovd <tg90nor@gmail.com>
2018-08-19 19:29:59 +0200

    Speed up users index page by preloading needed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.includes(:volunteers, :bands, :crew_application, :roles, :authentications)
 
     respond_to do |format|
       format.html # index.html.erb
