a76d5ab9d2841aee7288ed56054af10fe91505b0
Thomas Geirhovd <tg90nor@gmail.com>
2024-07-28 06:55:14 +0200

    Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port 3000
+if ENV.fetch("RAILS_ENV", "development") == "development"
+  port        ENV.fetch("PORT") { 3000 }
+else
+  bind "unix:/tmp/puma.sock"
+end
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -23,8 +27,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-#bind "unix:/tmp/puma.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
