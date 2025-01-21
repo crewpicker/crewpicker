fbf8827adf284a9128d5669d4e5411be0e348a59
Thomas Geirhovd <tg90nor@gmail.com>
2025-01-21 18:33:43 +0100

    Flake try two

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
 {
-  description = "A Nix-flake-based Node.js development environment";
+  description = "Development environment for your project";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-      });
-    in
-    {
-      overlays.default = final: prev: rec {
-        nodejs = prev.nodejs;
-        yarn = (prev.yarn.override { inherit nodejs; });
-      };
+  outputs = { self, nixpkgs }: {
+    devShell = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+        ruby
+        nodejs
+        yarn
+      ];
+      shellHook = ''
+        # Set ZSH as the default shell
+        export NIX_BUILD_SHELL="$SHELL"
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ ruby nodejs yarn ];
-        };
-      });
+        # Source your ZSH configuration files (if any)
+        if [ -f ~/.zshrc ]; then
+          source ~/.zshrc
+        fi
+      '';
     };
+  };
 }
