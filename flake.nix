4dea4e8f870de6f8be5adbeb4042ca4499128660
Thomas Geirhovd <tg90nor@gmail.com>
2025-01-21 20:53:04 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,24 @@
 {
-  description = "Development environment for your project";
+  description = "A Nix-flake-based Node.js development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }: {
-    devShell = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        ruby
-        nodejs
-        yarn
-      ];
-      shellHook = ''
-        # Set ZSH as the default shell
-        export NIX_BUILD_SHELL="$SHELL"
-
-        # Source your ZSH configuration files (if any)
-        if [ -f ~/.zshrc ]; then
-          source ~/.zshrc
-        fi
-      '';
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ruby nodejs yarn postgresql shared-mime-info ];
+          shellHook = ''
+            export FREEDESKTOP_MIME_TYPES_PATH="${pkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml"
+            export IN_NIXSHELL=yes
+          '';
+        };
+      });
     };
-  };
 }
