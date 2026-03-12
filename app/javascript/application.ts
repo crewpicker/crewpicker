2a5b3f16e311edbb892810d77336985a99b5d137
Thomas Geirhovd <tg90nor@gmail.com>
2026-03-13 00:54:56 +0100

    js import stuff

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "bootstrap/dist/js/bootstrap.esm.js"
+import "bootstrap"
 import "@fortawesome/fontawesome-pro/js/all"
 import { ImageResizer } from "./image-resizer"
 
